3299e2d3331cbf63150e1f33afc61cd2e1c9ddd3
ropok <jalerse@gmail.com>
2018-08-24 13:50:35 +0700

    First Update : Hellow World!

---
+++ 0-Hello-World/Hello.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Hello extends JFrame {
+    public Hello() {
+        super("hello");
+
+super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        super.add(new JLabel("Hellow, world!"));
+        super.pack();
+        super.setVisible(true);
+    }
+
+    public static void main(final String[] args){
+        new Hello();
+    }
+}
